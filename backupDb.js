9d1015faab73ec289c66950cd09715364043acba
shawn ning <zxning@gmail.com>
2020-10-22 10:32:02 -0400

    update

--- backupDb.js
+++ backupDb.js
@@ -24,17 +24,17 @@ function backupDb(){
 	log.info("backup started");	
 
 	let fname = `${host}-pris-${weekday}.bak`;
-	let bakfname = `${config.bakPath}${fname}`;
-	let zbakfname = `${config.bakPath}${fname}.7z`;
+	let backupFname = `${config.backupPath}${fname}`;
+	let zbackupFname = `${config.zbackupPath}${fname}.7z`;
 
 	//select top 1 * from pris.dbo.pos_sales order by date desc
 	if (config.runMode == 'wlm'){
 		runsql(`insert into pris.dbo.pos_sales(Product_ID,Quantity,Amount,Date,Notes) values ('1234567890',999,1,getdate(),'system')`);
 	}
 	
-	runsql(`BACKUP DATABASE [pris] TO DISK = N'${bakfname}' WITH INIT, NAME = N'pris-Database Backup ${dt}'`);
-	run(`7z a -t7z -mx9 -p2020 ${zbakfname} ${bakfname}`)
-	run(`rclone copy ${zbakfname} automan:mssqlbak-7z`)
+	runsql(`BACKUP DATABASE [pris] TO DISK = N'${backupFname}' WITH INIT, NAME = N'pris-Database Backup ${dt}'`);
+	run(`7z a -t7z -mx9 -p2020 ${zBackupFname} ${backupFname}`)
+	run(`rclone copy ${zbackupFname} automan:mssqlbak-7z`)
 }
 
 let host = os.hostname();
