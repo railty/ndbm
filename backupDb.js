36c6ca71394591c31b187d929d5bf5f21fa4331c
shawn ning <zxning@gmail.com>
2020-10-23 12:55:55 -0400

    update

--- backupDb.js
+++ backupDb.js
@@ -2,23 +2,23 @@ const config = require('./config.json');
 const {run, runsql, log, today, host} = require('./utils.js');
 const fs = require('fs'); 
 
-function backupDb(){
-	log.info("backup started");	
+function backupDb(db){
+	log.info(`backup ${db} started`);	
 
-	let fname = `${host}-pris-${today.weekday}.bak`;
+	let fname = `${host}-${db}-${today.weekday}.bak`;
 	let backupFname = `${config.backupPath}${fname}`;
 	let zBackupFname = `${config.zBackupPath}${fname}.7z`;
 
-	if (config.runMode == 'wlm'){
+	if (config.heartbeat){
 		runsql(`insert into pris.dbo.pos_sales(Product_ID,Quantity,Amount,Date,Notes) values ('1234567890',999,1,getdate(),'system')`);
 	}
 	
-	runsql(`BACKUP DATABASE [pris] TO DISK = N'${backupFname}' WITH INIT, NAME = N'pris-Database Backup ${today.ymd}'`);
+	runsql(`BACKUP DATABASE [${db}] TO DISK = N'${backupFname}' WITH INIT, NAME = N'${db}-Database Backup ${today.ymd}'`);
 	run(`7z a -t7z -mx9 -p2020 ${zBackupFname} ${backupFname}`)
 	run(`rclone copy ${zBackupFname} automan:mssqlbak-7z`)
 }
 
 for (let db of config.backupDbs){
-	backupDb();
+	backupDb(db);
 }
 
