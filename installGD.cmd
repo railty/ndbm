3f8bff74f373e0e3be79c29baf258cfa67eeaa11
shawn ning <zxning@gmail.com>
2020-11-20 11:20:10 -0500

    update

--- installGD.cmd
+++ installGD.cmd
@@ -1,3 +1,5 @@
+rem disable startup manually
+rem invertigate reg query HKCU\\Software... and wmic startup
 nssm.exe install gdrive "C:\Program Files\Google\Drive File Stream\43.0.8.0\GoogleDriveFS.exe"
 nssm set gdrive DisplayName gdrive
 nssm set gdrive ObjectName .\pris pass
