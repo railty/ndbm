61b7efb4aa425f5e0fd8a1eb5f045c5d51f91e8d
shawn ning <zxning@gmail.com>
2020-11-27 20:28:26 -0500

    update

---
+++ syncStore.js
@@ -0,0 +1,18 @@
+const config = require('./config.json');
+const {run, runsql, log, today, host} = require('./utils.js');
+const fs = require('fs'); 
+
+function syncStore(store){
+	log.info(`Sych ${store} started`);	
+	runsql(`Exec Pris.[dbo].[Pull_Store_Orders] @STORE = N'${store}'`)
+}
+
+try{
+	for (let store of ['alp', 'ofc', 'ofmm', 'wm1117', 'wm1116', 'wm3652', 'wm3135', 'wm1080']){
+		syncStore(store);
+	}
+}
+catch(e){
+	log.info(e.toString());
+}
+
