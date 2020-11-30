27469b6ce7ac02656c2d4f736373dc1c95d343c6
shawn ning <zxning@gmail.com>
2020-11-30 08:53:55 -0500

    update

--- backupDb.js
+++ backupDb.js
@@ -25,7 +25,15 @@ function backupDb(db){
 	
 	if (config.heartbeat){
 		if (config.heartbeat == 'wlm'){
-			run(`rclone copy ${zBackupFname} ${config.gPath} --log-file=log/r.log --log-level INFO`)
+
+			try{
+				run(`rclone copy ${zBackupFname} ${config.gPath} --log-file=log/r.log --log-level INFO`)
+			}
+			catch(e){
+				//if fail, try again
+				run(`rclone copy ${zBackupFname} ${config.gPath} --log-file=log/r.log --log-level INFO`)
+			}
+
 		}
 		if (config.heartbeat == 'alp'){
 			run(`copy ${zBackupFname} \"g:\\my drive\\DBBackup\\${host}\\\" `)
