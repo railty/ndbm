4a927fafb7b137f15c350caaa881fa21b0e927de
shawn ning <zxning@gmail.com>
2021-01-17 14:10:17 -0500

    update

--- syncStore.js
+++ syncStore.js
@@ -5,12 +5,15 @@ const fs = require('fs');
 function syncStore(store){
 	log.info(`Sych ${store} started`);	
 	runsql(`Exec Pris.[dbo].[Pull_Store_Orders] @STORE = N'${store}'`)
+	runsql(`Exec Pris.[dbo].[Pull_Store_Products] @STORE = N'${store}'`)
 }
 
 try{
 	for (let store of ['alp', 'ofc', 'ofmm', 'wm1117', 'wm1116', 'wm3652', 'wm3135', 'wm1080']){
 		syncStore(store);
 	}
+	
+	runsql(`Exec [dbo].[Push_Payment]`)
 }
 catch(e){
 	log.info(e.toString());
